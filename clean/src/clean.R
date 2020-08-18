cfd4d81666b459e9916ad69dcfc6de6f8395b3e3
JR <jessica.morgan.randall@emory.edu>
2020-08-18 17:25:41 -0400

    removed old notes, added new notes, moved two chunks to write task

--- clean/src/clean.R
+++ clean/src/clean.R
@@ -6,7 +6,7 @@
 # ============================================
 # HRW_pollingplaces/clean/src/clean.R
 #
-pacman::p_load("tidyverse", "assertr", "tidycensus")
+pacman::p_load("tidyverse", "assertr", "tidycensus", "janitor")
 
 inputs <- list(
   VIPinlist_imp = here::here("HRW_pollingplaces/clean/input/VIPdata_imported.rds"),
@@ -19,21 +19,16 @@ outputs <- list(
   az_2020_clean = here::here("HRW_pollingplaces/write/input/az_2020_clean.rds"),
   sc_2016_clean = here::here("HRW_pollingplaces/write/input/sc_2016_clean.rds"),
   sc_2020_clean = here::here("HRW_pollingplaces/write/input/sc_2020_clean.rds")
-  )
+)
 
 # VIP data
-# keep at county level for now
-
-# combine 2020 az data with maricopa-specific data
-# parse and create var for zip code, verify they are in range for each state
 
 n_last <- 5
 
-# verifications will break with new data
-# added zipcodes for several polling places with no zipcode data
-
 invalid_info <- c("4600331", "46002736", "4600617", "4600618", 
                   "4610205126","46002463", "4600178", "46003124")
+
+# nrows = how many unique polling places were open in 2016?
 
 az_2016_df <- pluck(read_rds(inputs$VIPinlist_imp), 1) %>%
   verify(ncol(.) == 3 & nrow(.) == 342) %>%
@@ -70,15 +65,22 @@ az_2016_df <- pluck(read_rds(inputs$VIPinlist_imp), 1) %>%
          zipcode = if_else(id == "46002737", "85346", zipcode)) %>%
   filter(!id %in% invalid_info) %>%
   verify(is.na(zipcode) == FALSE) %>%
-  group_by(address_line) %>%
-  slice(1) %>% 
-  ungroup()
+  filter(unique(id) != FALSE)
 
-#how many polling places were open in each zip code in 2016?
+# send to write task
 
+#FIXME: add unit tests for expected dimensions of each dataframe
+
+# nrows = number of unique zip codes in 2016 = 198
 az_zips_freq_2016 <- as.data.frame(table(az_2016_df$zipcode)) %>%
   mutate(zipcode = Var1, 
          n_pp_2016 = as.numeric(Freq))
+
+#FIXME: add unit tests
+
+# send to write task
+
+# 2020 data
 
 az_2020_df <- pluck(read_rds(inputs$VIPinlist_imp), 2) %>%
   verify(ncol(.) == 3 & nrow(.) == 330) %>%
@@ -88,14 +90,9 @@ az_2020_df <- pluck(read_rds(inputs$VIPinlist_imp), 2) %>%
                           nchar(address_line)))) %>%
   verify(is.na(zipcode) == FALSE) %>%
   filter(zipcode != "12345") %>%
-  group_by(address_line) %>%
-  slice(1) %>% 
-  ungroup()
+  filter(unique(id) != FALSE)
 
-no_overlap_az <- anti_join(az_2020_df, az_2016_df, by = "address_line")
-
-# there isn't much overlap between polling locationss open from 2016 to 2020, 
-# would we expect that most polling places would have changed completely?
+#FIXME: add unit tests
 
 az_2020_maricopa_df <- pluck(read_rds(inputs$VIPinlist_imp), 3) %>%
   verify(ncol(.) == 3 & nrow(.) == 151) %>%
@@ -104,45 +101,35 @@ az_2020_maricopa_df <- pluck(read_rds(inputs$VIPinlist_imp), 3) %>%
   mutate(zipcode = as.factor(substr(address_line, nchar(address_line) - n_last + 1, 
                           nchar(address_line)))) %>%
   verify(is.na(zipcode) == FALSE) %>%
-  group_by(address_line) %>%
-  slice(1) %>% 
-  ungroup()
+  filter(unique(id) != FALSE)
+
+#FIXME: add unit tests
+
+# nrows = how many unique polling places were open in 2020?
 
 az_2020_df_full <- full_join(az_2020_df, az_2020_maricopa_df) %>%
-  verify(ncol(.) == 4 & nrow(.) == 470) %>%
+  verify(ncol(.) == 4 & nrow(.) == 480) %>%
   verify(min(id) == 7700104067 & max(id) == 8850015622)  %>%
   mutate_at(c("id", "name", "address_line"), as.character) %>%
   mutate(zipcode = as.factor(substr(address_line, nchar(address_line) - n_last + 1, 
                           nchar(address_line)))) %>%
   verify(is.na(zipcode) == FALSE) %>%
-  filter(unique(id) != FALSE)  %>%
-  group_by(address_line) %>%
-  slice(1) %>% 
-  ungroup()
+  filter(unique(id) != FALSE)
 
+#FIXME: add unit tests
+# send to write task
+
+# nrows = number of unique zip codes in 2020 = 278
 az_zips_freq_2020 <- as.data.frame(table(az_2020_df_full$zipcode)) %>%
   mutate(zipcode = Var1, 
          n_pp_2020 = as.numeric(Freq))
+#FIXME: add unit tests
 
-# this suggests that there are more polling places in 2020 than 2016 but
-# do 2016 data also account for maricopa?
-
-# which zipcodes saw an increase, decrease, or maitenence in polling places?
-
-n_places_az <- full_join(az_zips_freq_2020, az_zips_freq_2016, by = "zipcode") %>%
-  replace_na(list(n_pp_2016 = 0, n_pp_2020 = 0)) %>%
-  mutate(delta_n_places = as.numeric(n_pp_2020 - n_pp_2016),
-         delta_cat = as.factor(if_else(delta_n_places < 0, 
-                                       "Fewer Polling Places", 
-                                       "The Same or More Polling Places"))) %>%
-  arrange(delta_n_places)
-
-# how many lost, gained, maintained?
-
-az_table <- table(n_places_az$delta_n_places)
+# send to write task
 
 ### south carolina ###
 
+#nrows = how many unique polling places were open in 2016?
 sc_2016_df <- pluck(read_rds(inputs$VIPinlist_imp), 4) %>%
   verify(ncol(.) == 3 & nrow(.) == 2194) %>%
   verify(min(id) == 88801000 & max(id) == 8880999) %>%
@@ -154,14 +141,16 @@ sc_2016_df <- pluck(read_rds(inputs$VIPinlist_imp), 4) %>%
     zipcode = as.character(if_else(zipcode == "23222", "29532", zipcode)),
     zipcode = as.factor(if_else(zipcode == "40 SC", "29840", zipcode))) %>%
   filter(zipcode != "	SC") %>%
-  group_by(address_line) %>%
-  slice(1) %>% 
-  ungroup()
+  filter(unique(id) != FALSE)
+#FIXME: add unit tests
 
+# nrows = number of unique zip codes in 2016 = 382
 sc_zips_freq_2016 <- as.data.frame(table(sc_2016_df$zipcode)) %>%
   mutate(zipcode = Var1, 
          n_pp_2016 = as.numeric(Freq))
+#FIXME: add unit tests
 
+#nrows = how many unique polling places were open in 2016? 1968
 sc_2020_df <- pluck(read_rds(inputs$VIPinlist_imp), 5) %>%
   verify(ncol(.) == 3 & nrow(.) == 1968) %>%
   verify(min(id) == 88801000 & max(id) == 8880999) %>%
@@ -171,72 +160,57 @@ sc_2020_df <- pluck(read_rds(inputs$VIPinlist_imp), 5) %>%
   verify(is.na(zipcode) == FALSE) %>%
   mutate(zipcode = as.factor(if_else(zipcode == "40 SC", "29840", zipcode))) %>%
   filter(zipcode != "SC") %>%
-  group_by(address_line) %>%
-  slice(1) %>% 
-  ungroup()
+  filter(unique(id) != FALSE)
+#FIXME: add unit tests
 
+# nrows = number of unique zip codes in 2020 = 383
 sc_zips_freq_2020 <- as.data.frame(table(sc_2020_df$zipcode)) %>%
   mutate(zipcode = Var1, 
          n_pp_2020 = as.numeric(Freq))
-
-# there are 815 different polling places in the 2020 dataset as compared to the
-# 2016 dataset
-
-no_overlap_sc <- anti_join(sc_2020_df, sc_2016_df, by = "address_line")
-
-# which zipcodes saw an increase, decrease, or maitenence in polling places?
-
-n_places_sc <- full_join(sc_zips_freq_2020, sc_zips_freq_2016, by = "zipcode") %>%
-  replace_na(list(n_pp_2016 = 0, n_pp_2020 = 0)) %>%
-  mutate(delta_n_places = as.numeric(n_pp_2020 - n_pp_2016),
-         delta_cat = as.factor(if_else(delta_n_places < 0, 
-                                       "Fewer Polling Places", 
-                                       "The Same or More Polling Places"))) %>%
-  filter(zipcode != "SC") %>%
-  arrange(delta_n_places)
-
-# how many lost, gained, maintained?
-
-sc_table <- table(n_places_sc$delta_n_places)
+#FIXME: add unit tests
+# send sc zips freq files to write task
 
 # add in census race data and link to each state/year dataset grouped by zip
-# for zips which lost polling places
 
-lost_places_az <- n_places_az %>%
-  filter(delta_cat == "Fewer Polling Places")
+az_demo <- read_rds(inputs$census_imp) %>% 
+  select(geoid, variable, estimate) %>% 
+  filter(geoid %in% n_places_az$zipcode) %>%
+  spread(key = "variable", value = "estimate") %>%
+  mutate(zip_pct_hl = as.numeric(round(signif_half_up((yes_h_l/total)*100), 1), 
+                                 digits = 2),
+         zip_pct_not_hl = as.numeric(round(signif_half_up((no_h_l/total)*100), 1), 
+                                     digits = 2)) %>%
+  group_by(geoid) %>%
+  full_join(n_places_az, by = c("geoid" = "zipcode"))
+#FIXME: add unit tests
 
-lost_places_sc <- n_places_sc %>%
-  filter(delta_cat == "Fewer Polling Places")
-
-az_demo_2016 <- pluck(read_rds(inputs$census_imp), 1) %>%
-  filter(geoid %in% lost_places_az$zipcode) %>%
-  group_by(geoid, variable)
-
-az_demo_2018 <- pluck(read_rds(inputs$census_imp), 2) %>%
-  filter(geoid %in% lost_places_az$zipcode) %>%
-  group_by(geoid)
-
-sc_demo_2016 <- pluck(read_rds(inputs$census_imp), 1) %>%
-  filter(geoid %in% lost_places_sc$zipcode) %>%
-  group_by(geoid)
-
-sc_demo_2018 <- pluck(read_rds(inputs$census_imp), 2) %>%
-  filter(geoid %in% lost_places_sc$zipcode) %>%
-  group_by(geoid)
+sc_demo <- pluck(read_rds(inputs$census_imp)) %>%
+  select(geoid, variable, estimate) %>% 
+  filter(geoid %in% n_places_sc$zipcode) %>%
+  spread(key = "variable", value = "estimate") %>%
+  mutate(zip_pct_hl = as.numeric(round(signif_half_up((yes_h_l/total)*100), 1), 
+                                 digits = 2),
+         zip_pct_not_hl = as.numeric(round(signif_half_up((no_h_l/total)*100), 1), 
+                                     digits = 2)) %>%
+  group_by(geoid) %>%
+  full_join(n_places_sc, by = c("geoid" = "zipcode"))
+#FIXME: add unit tests
+# export az and sc demo files to write task
 
 # import covid-19 related data
 
-expected_cols2 <- c("date", "county", "state", "fips", "cases", "deaths")
+expected_cols2 <- c("county", "state", "fips", "cases", "deaths")
 
-covid_data <- readRDS(input_covid$covid_imp) %>%
+covid_data <- readRDS(inputs$covid_imp) %>%
   clean_names() %>%
   verify(colnames(.) == expected_cols2) %>%
-  rename(date_rep = date) %>%
   filter(state == "Arizona" | state == "South Carolina") %>%
-  verify(ncol(.) == 6 & nrow(.) == 8971) %>%
+  group_by(state, county) %>%
+  summarize(cases_by_county = n())
+  verify(ncol(.) == 3 & nrow(.) == 63) %>%
   saveRDS(files$covid_imp)
-
-# create 1 rds which is a list of dataframes with each state/year combo 
-# which inclides zip, county, cases, demographics
+  
+#FIXME: add more unit tests
+# send to write task
 
 # done.
