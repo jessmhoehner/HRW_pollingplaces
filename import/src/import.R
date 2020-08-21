abb9f043d429e89992f3d59aa88a7b5454ad7b74
JR <jessica.morgan.randall@emory.edu>
2020-08-21 14:30:37 -0400

    chnaged from table 3002 to 3003 for more concise census data

--- import/src/import.R
+++ import/src/import.R
@@ -16,22 +16,22 @@ inputs <- list(
   az_2020 = here::here("import/input/vip_az_2020primary/polling_location.txt"),
   az_2020_maricopa = here::here("import/input/vip_az_maricopa_2020primary/polling_location.txt"),
   sc_2016 = here::here("import/input/vip_sc_2016primary/polling_location.txt"),
-  sc_2020 = here::here("import/input/vip_sc_2020primary/polling_location.txt"), 
+  sc_2020 = here::here("import/input/vip_sc_2020primary/polling_location.txt"),
   az_covid_zip = here::here("import/input/covid/COVID19CONFIRMED_BYZIP_excel.csv"))
 
 outputs <- list(
   VIPinlist_imp = here::here("clean/input/VIPdata_imported.rds"),
   covid_az_imp = here::here("clean/input/covid_az_imported.rds"),
-  census_imp = here::here("clean/input/census_imported.rds"), 
+  census_imp = here::here("clean/input/census_imported.rds"),
   geo_df = here::here("write/input/census_geometry_imported.rds")
   )
 
 # import VIP data
 ## creates a list of VIP files as connections
-inputslist <- list(inputs$az_2016, inputs$az_2020, inputs$az_2020_maricopa, 
+inputslist <- list(inputs$az_2016, inputs$az_2020, inputs$az_2020_maricopa,
                   inputs$sc_2016, inputs$sc_2020)
 
-names(inputslist) <- c("az_2016", "az_2020", "az_2020_maricopa", 
+names(inputslist) <- c("az_2016", "az_2020", "az_2020_maricopa",
                        "sc_2016", "sc_2020")
 
 stopifnot(length(inputslist) == 5)
@@ -39,17 +39,17 @@ stopifnot(length(inputslist) == 5)
 # verifications won't break with new data yet
 
 inlist <- lapply(inputslist, function(x) {
-  
+
   expected_cols <- c("id", "name", "address_line")
-  
-  x_df <- read_csv(x, col_names = TRUE, na = "NA", 
-                   col_types = cols_only(id = 'c', 
-                                         name = 'c', 
+
+  x_df <- read_csv(x, col_names = TRUE, na = "NA",
+                   col_types = cols_only(id = 'c',
+                                         name = 'c',
                                          address_line = 'c')) %>%
     clean_names() %>%
     verify(colnames(.) == expected_cols) %>%
     verify(ncol(.) == 3)
-  
+
 })
 
 stopifnot(length(inlist) == 5)
@@ -76,30 +76,17 @@ jrkey <- census_api_key("0e50711a6878668e3244305cfdd42faaa9e7a66c")
 
 expected_cols3 <- c("geoid", "name", "variable", "estimate", "moe")
 
-demo_2016 <- get_acs(geography = "zcta", 
-                     variables = c(total = "B03001_001", 
-                                   no_h_l = "B03001_002", 
-                                   yes_h_l = "B03001_012"), 
-                     year = 2018, 
-                     geometry = FALSE, 
+demo_1418 <- get_acs(geography = "zcta",
+                     variables = c(total = "B03003_001",
+                                   not_h_l = "B03003_002",
+                                   h_l = "B03003_003"),
+                     year = 2018,
+                     geometry = FALSE,
                      key = jrkey) %>%
   clean_names() %>%
   verify(colnames(.) == expected_cols3) %>%
   select(geoid, name, variable, estimate) %>%
   saveRDS(outputs$census_imp)
-
-geometry <- get_acs(geography = "zcta", 
-                    variables = c(total = "B03001_001"),
-                    year = 2018, 
-                    geometry = TRUE, 
-                    key = jrkey) %>%
-  clean_names() %>%
-  filter(geoid %in% az_covid_data$zipcode) %>%
-  verify(nrow(.) == 369) %>%
-  select(c(geoid, geometry)) %>%
-  group_by(geoid) %>%
-  verify(nrow(.) == 369 & ncol(.) == 2) %>%
-  saveRDS(outputs$geo_df)
 
 az_covid_data <- az_covid_data %>%
   saveRDS(outputs$covid_az_imp)
